480c35290232e17d66730e4e6ea5cb98e47183d7
Philip Van Raalte <phil.vanraalte@gmail.com>
2025-03-29 16:26:16 -0400

    switch to netlify

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,12 @@
-// app.config.ts
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "unenv";
 
 export default defineConfig({
   tsr: {
     appDirectory: "src",
   },
   server: {
-    preset: "cloudflare-pages",
-    unenv: cloudflare,
+    preset: "netlify",
     prerender: {
       routes: ["/"],
       crawlLinks: true,
