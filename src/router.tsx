4e0a2cae9082e9ee45f8f59ebc15eaccde245343
Philip Van Raalte <phil.vanraalte@gmail.com>
2025-04-08 20:25:38 -0400

    updated dependencies

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { routerWithQueryClient } from "@tanstack/react-router-with-query";
 import { routeTree } from "./routeTree.gen";
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
 import { NotFound } from "./components/NotFound";
+import { minutesToMilliseconds } from "date-fns";
 
 export interface RouterContext {
   queryClient: QueryClient;
@@ -15,6 +16,9 @@ export function createRouter() {
     routeTree,
     context: { queryClient } satisfies RouterContext,
     defaultPreload: "intent",
+    defaultPreloadDelay: 40,
+    // TODO - check for preload promiximity when they add it
+    defaultPreloadStaleTime: minutesToMilliseconds(15),
     scrollRestoration: true,
     defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: NotFound,
